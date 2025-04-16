f14fa50713948b4e527f0026da6c8cb31b7abfc9
Linch1 <m.carassiti@hotmail.it>
2025-04-16 19:22:19 +0200

    x

---
+++ inputs/test_drag_drop.js
@@ -0,0 +1,5 @@
+const Simulator = require("./Simulator");
+
+Simulator.fileDrag(
+    "/media/sf_automations/server/downloads/instagram/minetomeme/video/2932229522963100120_35921422568.mp4", 957, 594
+)
