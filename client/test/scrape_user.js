d51ac54f0f40d1751fa2277f37e7167d24eeb9ed
Linch1 <m.carassiti@hotmail.it>
2025-04-22 14:26:05 +0200

    fix

--- client/test/scrape_user.js
+++ client/test/scrape_user.js
@@ -1,13 +1,20 @@
 import MessageType from "../../shared/MessageType";
 import MessagesHandler from "../src/lib/master/MessagesHandler";
+import ExtensionWsServer from "../src/lib/extension/Ws.js";
 
-let username = "minetomeme";
-MessagesHandler[MessageType.OPEN_BROWSER](
-    { 
-        platform: "instagram", 
-        profile:"pixeledsign", 
-        username, 
-        feedUrl: `https://instagram.com/${username}`, 
-        reelsUrl: `https://instagram.com/${username}/reels/` 
-    }
-)
+console.log("starting extensions server")
+ExtensionWsServer.run();
+
+
+setTimeout(() => {
+    let username = "minetomeme";
+    MessagesHandler[MessageType.OPEN_BROWSER](
+        { 
+            platform: "instagram", 
+            profile:"pixeledsign", 
+            username, 
+            feedUrl: `https://instagram.com/${username}`, 
+            reelsUrl: `https://instagram.com/${username}/reels/` 
+        }
+    )
+}, 1000);
